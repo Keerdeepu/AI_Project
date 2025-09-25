f00211198830c0029c6dfb4560464349f7bbb230
M. Keerthi Reddy <136425048+Keerdeepu@users.noreply.github.com>
2025-09-25 19:47:56 +0530

    Delete UniBridge_Team2_Project directory

--- UniBridge_Team2_Project/frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://127.0.0.1:8000";
-
-
-export const searchInternships = async (data) => {
-  const response = await axios.post(`${API_URL}/search`, data);
-  return response.data.results;
-};
